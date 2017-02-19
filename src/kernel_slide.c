ca20643a069649c0f22ff72c57c5dc123a256c56
Brandon Azad <bazad@cs.stanford.edu>
2017-02-19 11:05:07 -0800

    Improve kernel slide detection on iOS
    libmemctl now uses a safer memory scan by default on iOS that seems
    safer on iOS 10.2. The unsafe linear memory scan was triggering panics
    on iOS 10.2 on an iPhone 5s. This new strategy scans only the kernel
    heap and (if correct) data within the kernel image.

--- src/kernel_slide.c
+++ src/kernel_slide.c
@@ -1,3 +1,7 @@
+#define KERNEL_PRIVATE 1
+#include "mach/vm_statistics.h"
+#undef KERNEL_PRIVATE
+
 #include "kernel_slide.h"
 
 #include "core.h"
@@ -23,15 +27,41 @@ is_kernel_slide(kword_t slide) {
 	return ((slide & ~0x000000007fe00000) == 0);
 }
 
+/*
+ * validate_kernel_slide
+ *
+ * Description:
+ * 	Pushes an error message onto the error stack if the computed kernel slide is not valid.
+ */
+static bool
+validate_kernel_slide() {
+	if (!is_kernel_slide(kernel_slide)) {
+		error_internal("computed slide 0x%016llx is not a valid kASLR slide",
+				kernel_slide);
+		return false;
+	}
+	return true;
+}
+
 #if KERNELCACHE
 
 static const uint64_t kernel_region_size_min = 0x0000000100000000;
 static const uint64_t kernel_region_size_max = 0x0000000104000000;
 static const int kernel_region_protection    = 0;
 
+static const kword_t slide_increment = 0x200000;
+static const kword_t max_slide       = 0x200000 * 0x200;
+
+/*
+ * find_kernel_region
+ *
+ * Description:
+ * 	Find the region of kernel memory containing the kernel mapping.
+ */
 static bool
 find_kernel_region(kaddr_t *region_base, kaddr_t *region_end) {
 	// Get the region containing the kernel.
+	*region_base = 0;
 	for (;;) {
 		mach_vm_size_t size = 0;
 		struct vm_region_basic_info_64 info;
@@ -43,6 +73,7 @@ find_kernel_region(kaddr_t *region_base, kaddr_t *region_end) {
 		if (kr != KERN_SUCCESS) {
 			error_internal("mach_vm_region(%p) failed: %s", *region_base,
 					mach_error_string(kr));
+			error_internal("could not find kernel region");
 			return false;
 		}
 		if (kernel_region_size_min <= size
@@ -55,24 +86,145 @@ find_kernel_region(kaddr_t *region_base, kaddr_t *region_end) {
 	}
 }
 
-static const kword_t slide_increment = 0x200000;
-static const kword_t max_slide       = 0x200000 * 0x200;
-
+/*
+ * read_word
+ *
+ * Description:
+ * 	Read a word of kernel memory, suppressing any errors.
+ */
 static bool
-kernel_slide_init_ios_unsafe() {
-	memctl_warning("using unsafe memory scan to locate kernel base");
-	kaddr_t region_base = 0;
-	kaddr_t region_end;
-	if (!find_kernel_region(&region_base, &region_end)) {
+read_word(kaddr_t addr, kword_t *value) {
+	error_stop();
+	kernel_io_result ior = kernel_read_word(kernel_read_unsafe, addr, value, sizeof(*value),
+			0);
+	error_start();
+	return ior == KERNEL_IO_SUCCESS;
+}
+
+/*
+ * check_kernel_base
+ *
+ * Description:
+ * 	Checks if the given address contains the kernel base. If so, sets the kernel_slide and
+ * 	returns true.
+ */
+static bool
+check_kernel_base(kaddr_t base) {
+	unsigned n = macho_header_size(&kernel.macho) / sizeof(kword_t);
+	const kword_t *kernel_header = (const kword_t *)kernel.macho.mh;
+	// Try to match the first n words.
+	for (unsigned i = 0;; i++) {
+		if (i == n) {
+			kernel_slide = base - kernel.base;
+			return true;
+		}
+		kword_t value;
+		bool success = read_word(base + i * sizeof(value), &value);
+		if (!success) {
+			return false;
+		}
+		if (value != kernel_header[i]) {
+			return false;
+		}
+	}
+}
+
+/*
+ * find_kernel_slide_from_heap_zone
+ *
+ * Description:
+ * 	Find the kernel slide given a possible heap zone_array element.
+ */
+static bool
+find_kernel_slide_from_heap_zone(kaddr_t zone_address, size_t zone_size,
+		kaddr_t kernel_region_base, kaddr_t kernel_region_end) {
+	// Read the pointer at the start of the possible zone element.
+	kaddr_t zone_array_ptr;
+	bool success = read_word(zone_address, &zone_array_ptr);
+	if (!success) {
 		return false;
 	}
+	if (zone_array_ptr <= kernel_region_base || kernel_region_end <= zone_array_ptr) {
+		return false;
+	}
+	// Check that the first pointer in the zone array points back to the zone.
+	kaddr_t ptr;
+	success = read_word(zone_array_ptr, &ptr);
+	if (!success || ptr != zone_address) {
+		return false;
+	}
+	// This looks like it might be a valid pointer within the kernel zone_array. Scan backwards
+	// looking for the kernel header.
+	ptr = zone_array_ptr & ~page_mask;
+	for (; ptr >= kernel_region_base; ptr -= page_size) {
+		if (check_kernel_base(ptr)) {
+			return true;
+		}
+	}
+	return false;
+
+}
+
+/*
+ * kernel_slide_init_ios_heap_scan
+ *
+ * Description:
+ * 	Find the kernel slide by scanning the heap. This is a hack, but it seems reasonably safe in
+ * 	practice.
+ *
+ * 	We scan the heap by looking for memory regions with the VM_KERN_MEMORY_ZONE tag at submap
+ * 	depth 0. On both iOS and macOS, there are 2 of these zones, and both contain as their first
+ * 	element a pointer into _zone_array.
+ */
+static bool
+kernel_slide_init_ios_heap_scan(kaddr_t kernel_region_base, kaddr_t kernel_region_end) {
+	mach_vm_address_t address = 0;
+	for (;;) {
+		// Get the next memory region.
+		mach_vm_size_t size = 0;
+		uint32_t depth = 0;
+		struct vm_region_submap_info_64 info;
+		mach_msg_type_number_t count = VM_REGION_SUBMAP_INFO_COUNT_64;
+		kern_return_t kr = mach_vm_region_recurse(kernel_task, &address, &size, &depth,
+			(vm_region_recurse_info_t)&info, &count);
+		if (kr != KERN_SUCCESS) {
+			return false;
+		}
+		// Skip regions that are not on the heap, in a submap, not readable and writable,
+		// or empty.
+		int prot = VM_PROT_READ | VM_PROT_WRITE;
+		if (info.user_tag != VM_KERN_MEMORY_ZONE
+		    || depth != 0
+		    || (info.protection & prot) != prot
+		    || info.pages_resident == 0) {
+			goto next;
+		}
+		// Check whether we can recover the kernel slide from this region.
+		bool success = find_kernel_slide_from_heap_zone(address, size, kernel_region_base,
+				kernel_region_end);
+		if (success) {
+			return true;
+		}
+next:
+		address += size;
+	}
+}
+
+/*
+ * kernel_slide_init_ios_unsafe_scan
+ *
+ * Description:
+ * 	Find the kernel slide by performing an unsafe memory scan. This seems to work on iOS
+ * 	10.1.1, but not on 10.2.
+ */
+static bool
+kernel_slide_init_ios_unsafe_scan(kaddr_t region_base, kaddr_t region_end) {
+	memctl_warning("using unsafe memory scan to locate kernel base");
 	// Scan memory to find the kernel base.
 	kword_t min_slide = 0;
 	if (region_base > kernel.base) {
 		min_slide = round2_up(region_base - kernel.base, slide_increment);
 	}
-	unsigned n = macho_header_size(&kernel.macho) / sizeof(kword_t);
-	const kword_t *kernel_header = (const kword_t *)kernel.macho.mh;
 	for (kword_t slide = min_slide; slide < max_slide; slide += slide_increment) {
 		kaddr_t base = kernel.base + slide;
 		assert(base >= region_base);
@@ -80,31 +232,32 @@ kernel_slide_init_ios_unsafe() {
 		if (base >= region_end - kernel.macho.size) {
 			break;
 		}
-		// Try to match the first n words.
-		for (unsigned i = 0;; i++) {
-			if (i == n) {
-				kernel_slide = base - kernel.base;
-				goto found;
-			}
-			kword_t value;
-			error_stop();
-			kernel_io_result ior = kernel_read_word(kernel_read_unsafe,
-					base + i * sizeof(value), &value, sizeof(value), 0);
-			error_start();
-			if (ior != KERNEL_IO_SUCCESS) {
-				break;
-			}
-			if (value != kernel_header[i]) {
-				break;
-			}
+		if (check_kernel_base(base)) {
+			return true;
 		}
 	}
-	error_internal("could not find kernel slide");
 	return false;
-found:
-	if (!is_kernel_slide(kernel_slide)) {
-		error_internal("computed slide 0x%016llx is not a valid kASLR slide",
-				kernel_slide);
+}
+
+/*
+ * kernel_slide_init_ios
+ *
+ * Description:
+ * 	Initialize the kernel slide on iOS.
+ */
+static bool
+kernel_slide_init_ios() {
+	kaddr_t region_base, region_end;
+	if (!find_kernel_region(&region_base, &region_end)) {
+		return false;
+	}
+	bool success = kernel_slide_init_ios_heap_scan(region_base, region_end)
+		|| kernel_slide_init_ios_unsafe_scan(region_base, region_end);
+	if (!success) {
+		error_internal("could not find kernel slide");
+		return false;
+	}
+	if (!validate_kernel_slide()) {
 		return false;
 	}
 	return true;
@@ -141,9 +294,7 @@ kernel_slide_init_macos() {
 	}
 	// Compute the kernel slide.
 	kernel_slide = (address + size) - round2_up(last, page_size);
-	if (!is_kernel_slide(kernel_slide)) {
-		error_internal("computed slide 0x%016llx is not a valid kASLR slide",
-				kernel_slide);
+	if (!validate_kernel_slide()) {
 		return false;
 	}
 	return true;
@@ -157,7 +308,7 @@ kernel_slide_init() {
 		return true;
 	}
 #if KERNELCACHE
-	return kernel_slide_init_ios_unsafe();
+	return kernel_slide_init_ios();
 #else
 	return kernel_slide_init_macos();
 #endif
